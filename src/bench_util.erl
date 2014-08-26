6624f75942108f76adadd5336d0127f08d03a2f7
Takeru Ohta <phjgt308@gmail.com>
2014-08-27 03:21:42 +0900

    first commit

---
+++ src/bench_util.erl
@@ -0,0 +1,19 @@
+%% @copyright 2014 Takeru Ohta <phjgt308@gmail.com>
+-module(bench_util).
+
+%% bench_util: bench_util library's entry point.
+
+-export([my_func/0]).
+
+
+%% API
+
+my_func() ->
+    ok().
+
+%% Internals
+
+ok() ->
+    ok.
+
+%% End of Module.
